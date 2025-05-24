fa470d46b393096d2e433d967a084163a8dc25cd
Ado <adonaylester85@gmail.com>
2025-05-24 11:08:48 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -98,9 +98,9 @@ global.catalogo = fs.readFileSync('./src/img/catagolo.jpg')
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
 //-------------------------[ IMAGEN ]------------------------------
-//global.img = "https://qu.ax/Zgqq.jpg"
-global.img1 = 'https://files.catbox.moe/rt8xqt.jpg'
-global.img2 = 'https://files.catbox.moe/6ve8db.jpg'
+//global.img = "https://qu.ax/ueLBh.jpg"
+global.img1 = 'https://qu.ax/vmBSN.jpg'
+global.img2 = 'hhttps://qu.ax/ueLBh.jpg'
 
 global.imagen = fs.readFileSync('./Menu2.jpg')
 global.imagen1 = fs.readFileSync('./media/Menu1.jpg')
